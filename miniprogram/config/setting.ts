6b0147bdc8b7eef74d2f3663f1267c12ed128379
aw <19972938+Bystander2@users.noreply.github.com>
2026-05-14 10:47:28 +0800

    feat: update version

--- miniprogram/config/setting.ts
+++ miniprogram/config/setting.ts
@@ -2,5 +2,5 @@ export default {
   API_BASE_URL: "https://search.aidimsum.com/api/miniprogram",
   API_PRIMARY_URL: "https://search.aidimsum.com/api/miniprogram/yue_cube_game",
   IS_REVIEW: false,
-  VERSION: "20260509",
+  VERSION: "20260514",
 };
